ac9a2def74721e7692ea7ff72be2dc1f430586d4
vikram-vivek <vikramvivek.v2@gmail.com>
2019-07-18 10:29:41 +0000

    docs.djangoproject.com/en/2.2/intro/tutorial03/#raising-a-404-error

--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
+from django.http import Http404
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list':latest_question_list,
-    }
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request,question_id):
-    return HttpResponse("You are looking at question %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'polls/details.html',{'question':question})
 
 def results(reuest,question_id):
     return HttpResponse("You are looking at results of question %s" % question_id)
