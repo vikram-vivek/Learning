31bf60bc4a5f8505749c46233f43ea9842aa58d7
vikram-vivek <vikramvivek.v2@gmail.com>
2019-07-18 12:46:57 +0000

    https://docs.djangoproject.com/en/2.2/intro/tutorial03/

--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 from django.http import Http404
@@ -10,10 +10,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request,question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request,'polls/details.html',{'question':question})
 
 def results(reuest,question_id):
