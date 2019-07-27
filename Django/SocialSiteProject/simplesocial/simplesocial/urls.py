0a34028001942eb92692e290c131b5a14fa0ef0b
Vikram Vivek <vikramvivek.v2@gmail.com>
2019-07-27 15:13:40 +0530

    Social_Site_Clone_Project_Part_14

--- Django/SocialSiteProject/simplesocial/simplesocial/urls.py
+++ Django/SocialSiteProject/simplesocial/simplesocial/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
+from . import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -27,3 +28,9 @@ urlpatterns = [
     url(r'^posts/',include('posts.urls',namespace='posts')),
     url(r'^groups/',include('groups.urls',namespace='groups'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/',include(debug_toolbar.urls))
+    ] + urlpatterns
