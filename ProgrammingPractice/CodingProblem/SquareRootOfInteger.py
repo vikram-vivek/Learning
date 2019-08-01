d9ea9198699a9fb92cb42538fc266a0321db1846
vikram-vivek <49717487+vikram-vivek@users.noreply.github.com>
2019-08-01 20:53:24 +0530

    https://www.geeksforgeeks.org/square-root-of-an-integer/
    Binary Search solution

--- ProgrammingPractice/CodingProblem/SquareRootOfInteger.py
+++ ProgrammingPractice/CodingProblem/SquareRootOfInteger.py
@@ -31,6 +31,31 @@ def floorSqrt(num):
         i = i+1
         result = i*i
     return i-1
+
+# A faster solution is Binary Search         
+def fastFloorSqrt(x): 
+    # Base cases 
+    if (x == 0 or x == 1): 
+        return x 
+    # Do Binary Search for floor(sqrt(x)) 
+    start = 1
+    end = x    
+    while (start <= end): 
+        mid = (start + end)//2
+        # If x is a perfect square 
+        if (mid*mid == x): 
+            return mid  
+        # Since we need floor, we update  
+        # answer when mid*mid is smaller 
+        # than x, and move closer to sqrt(x) 
+        if (mid * mid < x): 
+            start = mid + 1
+            ans = mid
+        else:  
+            # If mid*mid is greater than x 
+            end = mid-1 
+    return ans
+
 num = 11
-sol = floorSqrt(num)
+sol = fastFloorSqrt(num)
 print("Floor Sqrt of {} is {}".format(num,sol))
