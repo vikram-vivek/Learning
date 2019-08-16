072792b0f225f30a8c905cc7d6264a1316760c9c
vikram-vivek <49717487+vikram-vivek@users.noreply.github.com>
2019-08-16 23:39:00 +0530

    https://code.oursky.com/famous-egg-dropping-puzzle-in-combinatorics/

--- ProgrammingPractice/CodingProblem/Eggdrop.py
+++ ProgrammingPractice/CodingProblem/Eggdrop.py
@@ -1,1 +1,3 @@
 A building has 100 floors. One of the floors is the highest floor an egg can be dropped from without breaking.
+
+https://code.oursky.com/famous-egg-dropping-puzzle-in-combinatorics/
