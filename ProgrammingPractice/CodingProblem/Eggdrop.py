b5f4716300c474f02212e4504f610106b5a06824
vikram-vivek <49717487+vikram-vivek@users.noreply.github.com>
2019-08-19 23:52:05 +0530

    https://code.oursky.com/famous-egg-dropping-puzzle-in-combinatorics/

--- ProgrammingPractice/CodingProblem/Eggdrop.py
+++ ProgrammingPractice/CodingProblem/Eggdrop.py
@@ -1,3 +1,9 @@
-A building has 100 floors. One of the floors is the highest floor an egg can be dropped from without breaking.
+#A building has 100 floors. One of the floors is the highest floor an egg can be dropped from without breaking.
+#Assumptions in the Egg Dropping Puzzle:
+#The two eggs are identical.
+#If an egg does not break by dropping from a certain floor, it will not break dropping from any floor below that.
+#If an egg breaks by dropping on a certain floor, it will break dropping from any floor above that.
+#An egg may break by dropping on the 1st floor.
+#An egg may not break by dropping even on the 100th floor.
 
-https://code.oursky.com/famous-egg-dropping-puzzle-in-combinatorics/
+#https://code.oursky.com/famous-egg-dropping-puzzle-in-combinatorics/
