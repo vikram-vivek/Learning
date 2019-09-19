e34112827efbe0391b61b052e2b96c5bcb354a39
Vikram Vivek <vikramvivek.v2@gmail.com>
2019-09-19 12:30:25 +0530

    Added CRUD functionality

--- Django/ApiUsingDRF/djangorest/api/tests.py
+++ Django/ApiUsingDRF/djangorest/api/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from .models import Bucketlist
 
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
+
 # Create your tests here.
 class ModelTestCase(TestCase):
     """This class defines the test suite for the bucketlist model."""
@@ -16,3 +20,48 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+
+class ViewTestCase(TestCase):
+    """Test suite for api views"""
+
+    def setUp(self):
+        """Define the test client and other test variables"""
+        self.client = APIClient()
+        self.bucketlist_data = {'name':'Go to Ibiza'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format="json")
+
+    def test_api_can_create_a_bucketlist(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_bucketlist(self):
+        """Test the api can get a given bucketlist"""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.get(
+            reverse('details', kwargs={'pk':bucketlist.id}),
+            format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_api_can_update_bucketlist(self):
+        """Test the api can update a given bucketlist"""
+        bucketlist = Bucketlist.objects.get()
+        change_bucketlist = {'name': 'Something New'}
+        res = self.client.put(
+            reverse('details', kwargs={'pk':bucketlist.id}),
+            change_bucketlist,
+            format="json")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_bucket(self):
+        """Test the ai can delete a bucketlist"""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            format="json",
+            follow=True)
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
--- Django/ApiUsingDRF/djangorest/api/views.py
+++ Django/ApiUsingDRF/djangorest/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
 # Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the creae behaviour of our rest api"""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self,serializer):
+        """Save the post data when creating a new bucketlist"""
+        serializer.save()
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests"""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
